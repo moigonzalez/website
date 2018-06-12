3a20a30e6e4796935a8943a32f2778fed7be1066
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-12 06:20:14 +0000

    Deploy https://pwaawards.com to github.com/pwa-awards/website.git:gh-pages

---
+++ 10a9c85a.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const PREFIX = 'pwaawards.com';
+const HASH = '10a9c85a'; // Computed at build time.
+const OFFLINE_CACHE = `${PREFIX}-${HASH}`;
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(OFFLINE_CACHE).then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/favicon.ico',
+				'/screen.css',
+				'/script.js',
+				'/fonts/permanent-marker.woff',
+				'/fonts/permanent-marker.woff2',
+				'/images/111.png',
+				'/images/icon-228x228.png',
+				'/images/icon.svg',
+				'/apps/firefox.svg',
+				'/apps/flipboard.svg',
+				'/apps/flipkart.svg',
+				'/apps/starbucks.svg',
+				'/apps/teamwork.svg',
+				'/apps/telegram.svg',
+				'/apps/twitter.svg'
+			]); // Computed at build time.
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	// Delete old asset caches.
+	event.waitUntil(
+		caches.keys().then(function(keys) {
+			return Promise.all(
+				keys.map(function(key) {
+					if (
+						key != OFFLINE_CACHE &&
+						key.startsWith(`${PREFIX}-`)
+					) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	if (event.request.mode == 'navigate') {
+		console.log('Handling fetch event for', event.request.url);
+		console.log(event.request);
+		event.respondWith(
+			fetch(event.request).catch(function(exception) {
+				// The `catch` is only triggered if `fetch()` throws an exception,
+				// which most likely happens due to the server being unreachable.
+				console.error(
+					'Fetch failed; returning offline page instead.',
+					exception
+				);
+				return caches.open(OFFLINE_CACHE).then(function(cache) {
+					return cache.match('/');
+				});
+			})
+		);
+	} else {
+		// It’s not a request for an HTML document, but rather for a CSS or SVG
+		// file or whatever…
+		event.respondWith(
+			caches.match(event.request).then(function(response) {
+				return response || fetch(event.request);
+			})
+		);
+	}
+
+});
